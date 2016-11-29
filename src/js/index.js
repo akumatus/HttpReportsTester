70b6fd6df5bedbbdc136f813c4ed9dde6aaf4c8b
wuyue <wuyue07@baidu.com>
2016-11-29 15:27:17 +0800

    add promise

--- src/js/index.js
+++ src/js/index.js
@@ -18,13 +18,11 @@ var app = new Vue({
     url: 'https://0.baidu.com/',
     reportUrl: 'http://dr.w.baidu.com/report*',
     map: [],
-    code: '',
-    reports: []
+    code: [],
+    tabId: null
   },
 
-  created: function () {
-    this.webRequestListener();
-  },
+  created: function () {},
 
   methods: {
     chooseMapFile: function () {
@@ -57,56 +55,87 @@ var app = new Vue({
       for(i = 0; i < len; i++){
         switch (this.map[i].event){
           case 'scroll':
-            this.code.push(`
-            var scrollDoms = document.querySelectorAll('` + this.map[i].dom + `');
-            for (var i = 0; i < scrollDoms.length; i++){
-              scrollDoms[i].scrollTop = scrollDoms[i].offsetHeight;
-            }
-            `);
+            this.code.push({
+              name: this.map[i].event,
+              reports: this.map[i].reports,
+              code: `
+              var scrollDoms = document.querySelectorAll('${this.map[i].dom}');
+              for (var i = 0; i < scrollDoms.length; i++){
+                scrollDoms[i].scrollTop = scrollDoms[i].offsetHeight;
+              }`
+            });
             break;
           case 'click':
-            this.code.push(`
-            var clickDoms = document.querySelectorAll('` + this.map[i].dom + `');
-            var event;
-            for (var i = 0; i < clickDoms.length; i++){
-              event = new MouseEvent('click', {
-                'view': window,
-                'bubbles': true,
-                'cancelable': true
-              });
-              event.preventDefault();
-              clickDoms[i].dispatchEvent(event);
-            }
-            `);
+            this.code.push({
+              name: this.map[i].event,
+              reports: this.map[i].reports,
+              code: `
+              var clickDoms = document.querySelectorAll('${this.map[i].dom}');
+              var event;
+              for (var i = 0; i < clickDoms.length; i++){
+                event = new MouseEvent('click', {
+                  'view': window,
+                  'bubbles': true,
+                  'cancelable': true
+                });
+                event.preventDefault();
+                clickDoms[i].dispatchEvent(event);
+              }`
+            });
             break;
         }
       }
     },
+
     openWebsite: function () {
       var _this = this;
       chrome.tabs.create({
         url: this.url,
         active: false
       }, function (tab){
-        chrome.tabs.executeScript(tab.id, {code: _this.code[0]})
+        var i, len = _this.code.length;
+        var promiseArray = [];
+        this.tabId = tab.id;
+        for(i = 0; i < len; i++){
+          promiseArray.push(_this.executeScript(_this.code[i]));
+        }
+        Promise.all(promiseArray).then(function () {});
       });
     },
 
-    webRequestListener: function () {
+    executeScript: function (code) {
+      return new Promise((resolve, reject) => {
+        this.webRequestListener(code, resolve);
+        chrome.tabs.executeScript(this.tabId, {code: code.code});
+      });
+    },
+
+    webRequestListener: function (code, resolve) {
       var _this = this;
+      var distNum = code.reports.length;
+      var receivedNum = 0;
+      var reports = [];
+
       chrome.webRequest.onBeforeRequest.addListener(function(details) {
-        var reports = details.url.split('?')[1].split('&');
+        var reportRaw = details.url.split('?')[1].split('&');
         var report = {}, tempAry;
-        var i, len = reports.length;
+        var i, len = reportRaw.length;
+        
         for(i = 0; i < len; i++){
-          tempAry = reports[i].split('=');
+          tempAry = reportRaw[i].split('=');
           if(tempAry[0] !== '_'){ //filter timestamp
             report[tempAry[0]] = tempAry[1];
           }
         }
-        _this.reports.push(report);
-        console.log(_this.reports);
-        return {cancel: true};
+        reports.push(report);
+
+        receivedNum += 1;
+        if (receivedNum === distNum){
+          this.describe(reports, code); //mocha & chai assert
+          resolve(); //promise
+        }
+        
+        return {cancel: true}; //block http report sending
       }, {
         urls: [this.reportUrl]
       },
@@ -114,12 +143,14 @@ var app = new Vue({
       );
     },
 
-    describe: function () {
-      describe('Scroll', function() {
-        it('should start empty', function() {
-          var arr = [];
+    describe: function (reports, code) {
+      describe('scroll', function() {
+        it('should sending the same length of reports as expected', function() {
+          assert.lengthOf(reports.length, code.reports.length);
+        });
 
-          assert.equal(arr.length, 0);
+        it('should sending the same content as expected', function() {
+          
         });
       });
     },
